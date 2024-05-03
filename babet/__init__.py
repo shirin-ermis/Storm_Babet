d804e43228b409b3c3d9375d225160777df63b0e
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2024-05-03 15:26:14 +0100

    include met class to calculate meteorological variables

--- babet/__init__.py
+++ babet/__init__.py
@@ -8,3 +8,4 @@ from .version_info import VERSION_INT, VERSION  # noqa
 
 # Import main classes
 from .data import Data    # noqa
+from .met import Met      # noqa
---
+++ babet/met.py
@@ -0,0 +1,51 @@
+'''
+Functions to calculate meteorological variables
+'''
+
+import xarray as xr
+import os
+import numpy as np
+import pandas as pd
+import glob
+
+
+class Met():
+    """
+    Class to calculate meteorological variables.
+    """
+
+    def __init__(self):
+        self.status = None
+    
+    def calc_ivt(q, u, v, toplevel=300):
+        """
+        Function to calculate integrated vapor transport.
+        Definition to calculate integrated vapor transport.
+        Written by Shirin Ermis.
+
+        Input:
+        ------
+        q: xarray dataset
+            Specific humidity
+        u: xarray dataset
+            Zonal wind
+        v: xarray dataset
+            Meridional wind
+        lat: xarray dataset
+            Latitude
+        toplevel: int
+            Top level for integration
+
+        Output:
+        -------
+        ivt: xarray dataset
+            Integrated vapor transport
+        """
+
+        # Constants
+        g = 9.81
+
+        # Calculate IVT
+        vt = ((q * v)**2 + (q * u)**2)*(1/2) # vapour transport on all pressure levels
+        ivt = vt.sel(level=slice(1000, toplevel)).integrate('level') / g
+        return ivt
