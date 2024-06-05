0d8eb2b56e2942d59ae174e60762d8905bf7165e
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2024-06-05 19:15:59 +0100

    include babet_dist function

--- babet/lagrange.py
+++ babet/lagrange.py
@@ -64,3 +64,38 @@ class Lagrange():
         df['inidate'] = pd.to_datetime(inidate)
         df['number'] = int(mem)
         return df
+
+    def babet_dist(df):
+        '''
+        Function to calculate distance between Babet and tracks
+        Written by Nick Leach and Shirin Ermis.
+
+        Input:
+        ------
+        df: pandas dataframe, dataframe of tracks in IFS
+
+        Output:
+        ----
+        distances of tracks in IFS to Babet track in ERA5
+        '''
+        
+        # import tracks from ERA 5
+        era_track = pd.read_csv('/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/postproc/tracks/TEStitch_2023_0',skipinitialspace=True)
+        era_track['expid'] = 'era5'
+        era_track['experiment'] = 'era5'
+        era_track['inidate'] = pd.to_datetime('2023-10-01')
+        era_track['number'] = 0
+        era_track['date'] = pd.to_datetime(era_track.loc[:,['year','month','day','hour']])
+
+        # select Babet track from ERA5, get properties
+        babet_track = era_track.query('track_id==1')
+        babet_lons = babet_track.lon.values
+        babet_lats = babet_track.lat.values
+        
+        track_lons = df.lon.values
+        track_lats = df.lat.values
+        
+        # calculate distance between Babet and tracks in IFS
+        minsize = min(babet_lons.size,track_lons.size)
+        
+        return np.sqrt((track_lons[:minsize]-babet_lons[:minsize])**2+(track_lats[:minsize]-babet_lats[:minsize])**2).sum()
