8873f1807f657c7517a551759fa4fd81b5d12e37
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2025-04-09 16:16:52 +0100

    recalculate for ifs

--- data_scripts/map_plots_significance.py
+++ data_scripts/map_plots_significance.py
@@ -65,12 +65,7 @@ if __name__ == '__main__':
     # pgw['tp'] = (((pgw.tp.sel(time=slice('2023-10-19 00', '2023-10-22 00'))*3*3600).sum(dim='time'))/1e5).sel(lat=slice(uk[2], uk[3]), lon=slice(uk[0], uk[1]))
 
     # IFS
-    tmp = []
-    base_dir='/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/MED-R/EXP/{}/EU025/sfc/pf'
-    climates = ['1870', '1950', 'present', 'future1']
-    for e, exp in enumerate(['pi', 'pi_1950', 'curr', 'incr']):
-        tmp.append(xr.open_mfdataset(os.path.join(base_dir.format(exp), '*.nc'), engine='netcdf4', preprocess=preproc_ds_v2).expand_dims(climate=[climates[e]]))
-    ifs = xr.concat(tmp, dim='climate')
+    ifs = bb.data.Data.get_fba_ifs()
     ifs = ifs.sel(latitude=slice(uk[3], uk[2]), longitude=slice(uk[0], uk[1]))
     ifs['tp'] = ((ifs.tp.sel(time='2023-10-22 00') - ifs.tp.sel(time='2023-10-19 00'))*1000)
 
@@ -81,17 +76,17 @@ if __name__ == '__main__':
     # Bootstrapping ------------------------------
     
     # ERA5
-    print('Now calculating for ERA5')
-    bootstrapped = xr.apply_ufunc(
-        bootstrap_sample, 
-        era5_analogues.tp-era5_analogues.tp.sel(climate='present'), 
-        input_core_dims=[['member']],  # Modify based on your data dimensions
-        vectorize=True,  # Ensures element-wise computation
-        dask="parallelized",  # Enables parallel execution if data is chunked
-        output_core_dims=[["percentile"]],  # Output contains percentiles
-    )
-    era5_analogues_sign = bootstrapped.assign_coords(percentile=[2.5, 97.5]).to_netcdf('/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/postproc/significance/era5_analogues_tp_sign_map.nc')
-    # print(f"Size in memory: {era5_analogues_sign.nbytes / 1024**2:.2f} MB")
+    # print('Now calculating for ERA5')
+    # bootstrapped = xr.apply_ufunc(
+    #     bootstrap_sample, 
+    #     era5_analogues.tp-era5_analogues.tp.sel(climate='present'), 
+    #     input_core_dims=[['member']],  # Modify based on your data dimensions
+    #     vectorize=True,  # Ensures element-wise computation
+    #     dask="parallelized",  # Enables parallel execution if data is chunked
+    #     output_core_dims=[["percentile"]],  # Output contains percentiles
+    # )
+    # era5_analogues_sign = bootstrapped.assign_coords(percentile=[2.5, 97.5]).to_netcdf('/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/postproc/significance/era5_analogues_tp_sign_map.nc')
+    # # print(f"Size in memory: {era5_analogues_sign.nbytes / 1024**2:.2f} MB")
 
     # IFS
     print('Now calculating for IFS')
@@ -108,17 +103,17 @@ if __name__ == '__main__':
     # print(f"Size in memory: {ifs_sign.nbytes / 1024**2:.2f} MB")
 
     # MICAS
-    print('Now calculating for MICAS')
-    bootstrapped = xr.apply_ufunc(
-        bootstrap_sample,
-        micas.tp-micas.tp.sel(climate='present'),
-        input_core_dims=[['member']],
-        vectorize=True,
-        dask="parallelized",
-        output_core_dims=[["percentile"]],
-    )
-    micas_sign = bootstrapped.assign_coords(percentile=[2.5, 97.5]).to_netcdf('/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/postproc/significance/micas_tp_sign_map.nc')
-    # print(f"Size in memory: {micas_sign.nbytes / 1024**2:.2f} MB")
+    # print('Now calculating for MICAS')
+    # bootstrapped = xr.apply_ufunc(
+    #     bootstrap_sample,
+    #     micas.tp-micas.tp.sel(climate='present'),
+    #     input_core_dims=[['member']],
+    #     vectorize=True,
+    #     dask="parallelized",
+    #     output_core_dims=[["percentile"]],
+    # )
+    # micas_sign = bootstrapped.assign_coords(percentile=[2.5, 97.5]).to_netcdf('/gf5/predict/AWH019_ERMIS_ATMICP/Babet/DATA/postproc/significance/micas_tp_sign_map.nc')
+    # # print(f"Size in memory: {micas_sign.nbytes / 1024**2:.2f} MB")
 
     # # RACMO
     # print('Now calculating for RACMO')
