ca831b0e9dd5f17b8758144f2201f01e96feacca
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2025-10-08 17:26:33 +0100

    vert w average

--- babet/met.py
+++ babet/met.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 import pandas as pd
 import glob
+import metpy.calc as mpcalc
+from metpy.units import units
 
 
 class Met():
@@ -71,3 +73,24 @@ class Met():
             np.cos(lat1) * np.cos(lat2) * np.sin((lon2-lon1)/2.0)**2
 
         return earth_radius * 2 * np.arcsin(np.sqrt(a))
+    
+    def calc_vert_velocity_average(w, levels, temperature, Pa_to_mm=True,
+                                   upper=200, lower=850): 
+        
+        # look out for unites of temperature and pressure!
+        assert(temperature.mean().values > 200, 'Temperature needs to be in K')
+        assert(levels.mean().values < 1000, 'Pressure levels need to be in hPa')
+
+        # convert to mm
+        if Pa_to_mm:
+            vert_w = mpcalc.vertical_velocity(w*units('Pa/s'), levels*units('hPa'), temperature*units('K'))
+
+        # mass weighted mean
+        delta_p = vert_w.level.diff('level').rename('delta_p')
+        delta_p = delta_p.assign_coords(p=vert_w['level'].isel(level=slice(1, None)))
+        weighted_w  = vert_w.sel(level=slice(upper, lower)).isel(level=slice(1, None)) * delta_p
+        av_w = (weighted_w.sum(dim='level') / delta_p.sum(dim='level')).rename('av_w') * 100
+
+        av_w.assign_attrs(units='mm/s', description=f'Mass averaged vertical velocity between {upper}hPa and {lower}hPa')
+
+        return av_w
